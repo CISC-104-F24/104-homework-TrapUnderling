93cbf1d507016909ea2309c238d0c8724b44e55f
TrapUnderling <masont@udel.edu>
2024-09-06 17:03:59 -0400

    Completes Debug Scripts
    Adds Debug.LogWarning and Debug.LogError with their respective messages alongside multi-line comments that will be skipped by the code.

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -8,6 +8,14 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World");
+        Debug.LogWarning("WARNING! BE CAREFUL WITH PROCEEDINGS!");
+        Debug.LogError("There has been an error. Please try again.");
+        //Author: Mason Toothman
+        /*
+         * How would you import outside images into unity and create separate character dialogues?
+         * I have an idea to make a somewhat dark visual novel with heavy undertones depicting mental health; think of it somewhat like Omori as an example, but without the happy-go-lucky parts in a way.
+         * I hope to learn more about particles or scripting while in this class.
+         */
     }
 
     // Update is called once per frame
